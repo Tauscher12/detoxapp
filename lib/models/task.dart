f13620cf8a63acf0e2b8ae0bb1faf7c2d293e4fb
wolfi <wolfitau@gmail.com>
2024-12-27 13:41:15 +0100

    Add JSON serialization to Task model and implement data loading in TaskProvider

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -8,4 +8,19 @@ class Task {
     required this.title,
     required this.isDone,
   });
+
+    factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      isDone: json['isDone'] ?? false,
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'isDone': isDone,
+    };
+  }
 }
